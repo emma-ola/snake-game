98c6827e2faa2bed1efaa357deb92c5de3763014
Emmanuel <emma@example.com>
2021-05-23 19:26:19 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Classic Snake Game
+Similar implementation of the snake game found on the earliest Nokia 3310
---
+++ snake.py
@@ -0,0 +1,56 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape='square')
+        new_segment.color('white')
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend_snake(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for segment_num in range(len(self.segments) - 1, 0, -1):
+            # Get the current pos of the segment before the current segment and store it in a variable
+            new_xcor = self.segments[segment_num - 1].xcor()
+            new_ycor = self.segments[segment_num - 1].ycor()
+
+            # Move the current segment to the position of the segment before it.
+            self.segments[segment_num].goto(x=new_xcor, y=new_ycor)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
