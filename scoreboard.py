623c1e11073e36b1feda106d9de86dd988524add
Emmanuel <emma@example.com>
2021-05-25 10:00:48 -0400

    Added Data Persistence

--- scoreboard.py
+++ scoreboard.py
@@ -3,13 +3,16 @@ from turtle import Turtle
 ALIGNMENT = 'center'
 FONT = ("Courier", 24, "normal")
 
+with open(file='data.txt', mode='r') as file:
+    contents = file.read()
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = int(contents)
         self.color('blue')
         self.penup()
         self.goto(x=0, y=260)
@@ -23,6 +26,8 @@ class Scoreboard(Turtle):
     def set_high_score(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open(file='data.txt', mode='w') as file_w:
+                file_w.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 
